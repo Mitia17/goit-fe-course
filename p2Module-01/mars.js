af3fbc6af81ecdc4408bf8b4b5f6b45319cbe5bd
Mitia17 <mitia200076@gmail.com>
2019-06-24 21:09:00 +0300

    sorry late

---
+++ p2Module-01/mars.js
@@ -0,0 +1,40 @@
+'use strict'
+
+// const Password = 'ADMIN_PASSWORD'
+
+// console.log(Password);
+
+// var message = prompt('Enter Password');
+
+// if (message.toLowerCase() == "ADMIN_PASSWORD") {
+
+//     alert("Добро пожаловать!");
+
+//   }
+
+// const message = 'ADMIN_PASSWORD'
+
+// var response = prompt("Login", message) 
+
+// if (confirm("Добро пожаловать!")) {
+
+//     txt = "You pressed OK!";
+
+//   } else {
+
+//     txt = "You pressed Cancel!";
+
+//   }
+
+if (top.location.search=="") { 
+
+   const pass = prompt('Введите пароль'); 
+
+    if (pass=='ADMIN_PASSWORD') 
+
+     alert('Добро пожаловать!') 
+
+      else  alert('Доступ запрещен, неверный пароль!')}
+
+      
+
