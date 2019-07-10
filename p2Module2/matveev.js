aba7fcf7fb9571215d190a9474c4979f48bd2c6a
Mitia17 <mitia200076@gmail.com>
2019-07-10 20:18:33 +0300

    It's a living thing

--- p2Module2/matveev.js
+++ p2Module2/matveev.js
@@ -1,43 +1,5 @@
 'use strict'
 
-// const Password = 'ADMIN_PASSWORD'
-
-// console.log(Password);
-
-// var message = prompt('Enter Password');
-
-// if (message.toLowerCase() == "ADMIN_PASSWORD") {
-
-//     alert("Добро пожаловать!");
-
-//   }
-
-// const message = 'ADMIN_PASSWORD'
-
-// var response = prompt("Login", message) 
-
-// if (confirm("Добро пожаловать!")) {
-
-//     txt = "You pressed OK!";
-
-//   } else {
-
-//     txt = "You pressed Cancel!";
-
-//   }
-
-// if (top.location.search=="") { 
-
-//    const pass = prompt('Введите пароль'); 
-
-//     if (pass=='ADMIN_PASSWORD') 
-
-//      alert('Добро пожаловать!') 
-
-//       else  alert('Доступ запрещен, неверный пароль!')}
-
-      
-
 let attemptsLeft = 1;
 const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 var password; 
